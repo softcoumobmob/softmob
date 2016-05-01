fda301b319474d2fea806475bbf28feddb100a25
444495 <easycomeseasybooooom@icloud.com>
2016-05-02 07:37:47 +0900

    Bunnyクラスを定義

---
+++ src/se2/bunny/Bunny.java
@@ -0,0 +1,19 @@
+package se2.bunny;
+
+import com.jogamp.opengl.GLAutoDrawable;
+
+/**
+ * スタンフォードうさぎのモデルそのものです。利用側は{@code draw}メソッドを呼び出すだけで描画できるようにします。
+ * つまり、{@code TestEventListener}で言うところの{@code glut.glutWireTeapot(scale)}を、{@code bunny.draw(gl2)}に置き換えられるようにします。
+ * @author kawabata
+ *
+ */
+public class Bunny extends SolidFigure{
+
+	@Override
+	public void draw(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
