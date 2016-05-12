03a267fdbc42b54def21551f76617052fe8778a8
YukiMOB <yuki.rx0126@gmail.com>
2016-05-12 11:36:25 +0900

    改行したんご
    我・改行なり

--- Hello_World_github.c
+++ Hello_World_github.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-  printf("Hello World Github!!");//カンマ忘れんぞwww
-  printf("Hello GUNDAM");
+  printf("Hello World Github!!\n");//カンマ忘れんぞwww
+  printf("Hello GUNDAM\n");//開業したんご
   exit(0);
 }
