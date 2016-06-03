c1ee434d2ab270709233f27dc982489286462bd9
neet <kimura2@kimuratomotsune-no-MacBook-Air.local>
2016-06-03 10:09:02 +0900

    気にしないで

--- src/se2/bunny/Bunny.java
+++ src/se2/bunny/Bunny.java
@@ -1,6 +1,7 @@
 package se2.bunny;
 
 import com.jogamp.opengl.GL2;
+import java.util.ArrayList;
 
 /**
  * スタンフォードうさぎのモデルそのものです。利用側は{@code draw}メソッドを呼び出すだけで描画できるようにします。
@@ -11,28 +12,12 @@ import com.jogamp.opengl.GL2;
  */
 public class Bunny extends SolidFigure{
 	
-	PLYSource ply;
+    PLYSource ply;
 
 	@Override
 	public void draw(GL2 gl2) {
 		// TODO Auto-generated method stu
 
-	    /*gl2.glClear(GL.GL_COLOR_BUFFER_BIT);
-	
-	gl2.glPushMatrix();
-	gl2.glRotatef(angleX, 1.0f, 0.0f, 0.0f);
-	gl2.glRotatef(angleY, 0.0f, 1.0f, 0.0f);
-	gl2.glScalef(scale, scale, scale);
-	for(TriangleFace face: triangles){
-		gl2.glBegin(GL2.GL_POLYGON);
-		Vertex3D[] vertexs = face.toArray();
-		for(Vertex3D vertex: vertexs){
-			gl2.glVertex3f(vertex.getX()-x_cen, vertex.getY()-y_cen, vertex.getZ()-z_cen);
-			
-		}
-		gl2.glEnd();
-	}
-	gl2.glPopMatrix();*/
 	}
     
 }
